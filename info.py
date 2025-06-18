b7743fb4985d8d6d806843c37d040e4062266c8f
laiiiq <laiiqlaiiq033@gmail.com>
2025-06-18 18:31:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "7581698581:AAGvHYnGBoh8WdKuU9CEFfL2ICf1y13
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', '')).split()
 
 
 # Admins & Users
